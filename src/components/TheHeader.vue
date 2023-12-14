ebb2c2c8e543abcfa71bfa38760e58811897114a
Alexey5689 <jjookkeerrr56@gmail.com>
2023-12-14 18:31:57 +0500

    test

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,148 +1,165 @@
 <script setup>
-import { RouterLink } from 'vue-router'
-import IconOrderProject from '@/components/icons/IconOrderProject.vue';
+import { RouterLink } from "vue-router";
+import IconOrderProject from "@/components/icons/IconOrderProject.vue";
 </script>
 
 <script>
 export default {
-    data() {
-        return {
-            isDropdownOpened: false,
-        };
+  data() {
+    return {
+      isDropdownOpened: false,
+    };
+  },
+  methods: {
+    toggleDropdown() {
+      this.isDropdownOpened = !this.isDropdownOpened;
     },
-    methods: {
-        toggleDropdown() {
-            this.isDropdownOpened = !this.isDropdownOpened;
-        },
-        closeDropdown() {
-            this.isDropdownOpened = false;
-        },
+    closeDropdown() {
+      this.isDropdownOpened = false;
     },
+  },
 };
 </script>
 
-
-
-
-
 <template>
-    <header class="header">
+  <header class="header">
+    <!--  -->
+    <div class="header__dropdown" :class="{ opened: isDropdownOpened }">
+      <div class="left">
+        <div class="left-part">
+          <nav>
+            <ul>
+              <li>О компании</li>
+              <li>
+                <RouterLink to="/portfolio">Портфолио</RouterLink>
+              </li>
+              <li>
+                <RouterLink to="/services">Услуги</RouterLink>
+              </li>
+            </ul>
+          </nav>
+          <p>IT-компания lumos.ru, 2023 © Все права защищены</p>
+        </div>
+        <div class="right-part">
+          <nav>
+            <ul>
+              <li>
+                <a class="btn-order"
+                  >Заказать проект
+                  <svg
+                    width="70"
+                    height="70"
+                    viewBox="0 0 33 32"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M27.8027 8L12.5 23.3027M27.8027 8L27.8027 21.3456M27.8027 8L14.4816 8"
+                      stroke="#FD8B93"
+                      stroke-width="2"
+                      stroke-linejoin="round"
+                    />
+                  </svg>
+                </a>
+              </li>
+              <li>Контакты</li>
+            </ul>
+          </nav>
 
-        <!--  -->
-        <div class="header__dropdown" :class="{ opened: isDropdownOpened }">
-            <div class="left">
-                <div class="left-part">
-                    <nav>
-                        <ul>
-                            <li>О компании</li>
-                            <li>
-                                <RouterLink to="/portfolio">Портфолио</RouterLink>
-                            </li>
-                            <li>
-                                <RouterLink to="/services">Услуги</RouterLink>
-                            </li>
-                        </ul>
-                    </nav>
-                    <p>IT-компания lumos.ru, 2023 © Все права защищены</p>
-                </div>
-                <div class="right-part">
-                    <nav>
-                        <ul>
-                            <li>
-                                <a class="btn-order">Заказать проект
-                                    <svg width="70" height="70" viewBox="0 0 33 32" fill="none"
-                                        xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M27.8027 8L12.5 23.3027M27.8027 8L27.8027 21.3456M27.8027 8L14.4816 8"
-                                            stroke="#FD8B93" stroke-width="2" stroke-linejoin="round" />
-                                    </svg>
-                                </a>
-                            </li>
-                            <li>Контакты</li>
-                        </ul>
-                    </nav>
+          <div class="right-contacts">
+            <p>+7 (912) 345-67-89</p>
+            <span class="dropdown__divider"></span>
+            <p>it@lumos.ru</p>
+          </div>
+        </div>
+      </div>
 
-                    <div class="right-contacts">
-                        <p>+7 (912) 345-67-89</p>
-                        <span class="dropdown__divider"></span>
-                        <p>it@lumos.ru</p>
-                    </div>
-                </div>
-            </div>
+      <div class="left-mobile">
+        <nav>
+          <ul>
+            <li>
+              <a class="btn-order"
+                >Заказать проект
+                <svg
+                  width="70"
+                  height="70"
+                  viewBox="0 0 33 32"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M27.8027 8L12.5 23.3027M27.8027 8L27.8027 21.3456M27.8027 8L14.4816 8"
+                    stroke="#FD8B93"
+                    stroke-width="2"
+                    stroke-linejoin="round"
+                  />
+                </svg>
+              </a>
+            </li>
+            <li>Контакты</li>
+            <li>О компании</li>
+            <li>Портфолио</li>
+            <li>Услуги</li>
+          </ul>
+        </nav>
 
-            <div class="left-mobile">
-                <nav>
-                    <ul>
-                        <li>
-                            <a class="btn-order">Заказать проект
-                                <svg width="70" height="70" viewBox="0 0 33 32" fill="none"
-                                    xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M27.8027 8L12.5 23.3027M27.8027 8L27.8027 21.3456M27.8027 8L14.4816 8"
-                                        stroke="#FD8B93" stroke-width="2" stroke-linejoin="round" />
-                                </svg>
-                            </a>
-                        </li>
-                        <li>Контакты</li>
-                        <li>О компании</li>
-                        <li>Портфолио</li>
-                        <li>Услуги</li>
-                    </ul>
-                </nav>
-
-                <div class="left-mobile-contacts">
-                    <p>+7 (912) 345-67-89</p>
-                    <p>it@lumos.ru</p>
-                </div>
-
-                <p class="left-mobile-rights">IT-компания lumos.ru, 2023 © Все права защищены</p>
-
-            </div>
-            <div class="right">
-                <button class="close-menu" @click="closeDropdown">
-                    <img src="@/assets/img/icons/icons.svg#x" alt="Закрыть">
-                </button>
-                <div class="dropdown__links">
-                    <a href="">ВКонтакте</a>
-                    <a href="">Telegram</a>
-                    <a href="">Behance</a>
-                    <a href="">Dribbble</a>
-                </div>
-            </div>
+        <div class="left-mobile-contacts">
+          <p>+7 (912) 345-67-89</p>
+          <p>it@lumos.ru</p>
         </div>
 
-        <!--  -->
-        <div class="wrapper">
-            <ul class="header__left-side">
-                <li class="header__menu" @click="toggleDropdown">
-                    <p>Меню</p>
-                    <button>
-                        <span></span>
-                    </button>
-                </li>
-                <li class="header__left-item">
-                    <RouterLink to="/services">Услуги</RouterLink>
-                </li>
-                <li class="header__left-item">
-                    <RouterLink to="/portfolio">Портфолио</RouterLink>
-                </li>
-            </ul>
-            <div class="header__logo">
-                <RouterLink to="/">
-                    <img src="@/assets/img/icons/icons.svg#logo" alt="Логотип">
-                </RouterLink>
-            </div>
-            <ul class="header__right-side">
-                <li class="header__right-item">+7 (919) 123 45 67</li>
-                <li>
-                    <a class="btn-order">Заказать проект
-                        <IconOrderProject />
-                    </a>
-                </li>
-            </ul>
+        <p class="left-mobile-rights">
+          IT-компания lumos.ru, 2023 © Все права защищены
+        </p>
+      </div>
+      <div class="right">
+        <button class="close-menu" @click="closeDropdown">
+          <img src="@/assets/img/icons/icons.svg#x" alt="Закрыть" />
+        </button>
+        <div class="dropdown__links">
+          <a href="">ВКонтакте</a>
+          <a href="">Telegram</a>
+          <a href="">Behance</a>
+          <a href="">Dribbble</a>
         </div>
+      </div>
+    </div>
 
-        <!--  -->
+    <!--  -->
+    <div class="wrapper">
+      <ul class="header__left-side">
+        <li class="header__menu" @click="toggleDropdown">
+          <p>Меню</p>
+          <button>
+            <span></span>
+          </button>
+        </li>
+        <li class="header__left-item">
+          <RouterLink to="/services">Услуги</RouterLink>
+        </li>
+        <li class="header__left-item">
+          <RouterLink to="/portfolio">Портфолио</RouterLink>
+        </li>
+      </ul>
+      <div class="header__logo">
+        <RouterLink to="/">
+          <img src="@/assets/img/icons/icons.svg#logo" alt="Логотип" />
+        </RouterLink>
+      </div>
+      <ul class="header__right-side">
+        <li class="header__right-item">+7 (919) 123 45 67</li>
+        <li>
+          <a class="btn-order"
+            >Заказать проект
+            <IconOrderProject />
+          </a>
+        </li>
+      </ul>
+    </div>
 
-        <!-- <div class="dark-back">
+    <!--  -->
+
+    <!-- <div class="dark-back">
             <div class="form-wrapper">
                 <button class="close-form">
                     <img src="img/icons/icons.svg#x" alt="Закрыть">
@@ -340,26 +357,24 @@ export default {
                 </svg>
             </div>
         </div> -->
-
-
-    </header>
+  </header>
 </template>
 
 <style scoped>
 .header__menu button {
-    cursor: pointer;
+  cursor: pointer;
 }
 
 .header__left-item a,
 .header__logo a,
 .header .left-part nav ul li a {
-    text-decoration: none;
-    color: #1E1E1E;
+  text-decoration: none;
+  color: #1e1e1e;
 }
 
 .header__logo a img {
-    display: -webkit-box;
-    display: -ms-flexbox;
-    display: flex;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
 }
 </style>
