8a79c6244145853feeea587795a02d00651ab9c5
Alexey5689 <jjookkeerrr56@gmail.com>
2023-12-15 16:21:04 +0500

    динам title

--- src/router/index.js
+++ src/router/index.js
@@ -8,26 +8,34 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: HomeView,
+      meta: { title: "Lymous" },
     },
     {
       path: "/services",
       name: "services",
       component: () => import("@/views/ServicesView.vue"),
+      meta: { title: "Services" },
     },
     {
       path: "/portfolio",
       name: "portfolio",
       component: () => import("@/views/PortfolioView.vue"),
+      meta: { title: "Portfolio" },
     },
     {
       path: "/services/development",
       name: "development",
       component: () => import("@/views/DevelopmentView.vue"),
+      meta: { title: "Development" },
     },
   ],
   scrollBehavior(to, from, savedPosition) {
     return savedPosition || { top: 0 };
   },
+});
+router.beforeEach((to, from, next) => {
+  document.title = `${to.meta.title}`;
+  next();
 });
 
 export default router;
