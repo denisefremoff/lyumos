4f1faeab6ff8f403538c64d8555a4f176f465d33
denisefremoff <denisefremoff@mail.ru>
2023-12-15 17:32:59 +0500

    динамический заголовок страницы

--- src/router/index.js
+++ src/router/index.js
@@ -8,31 +8,40 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: HomeView,
+      meta: { title: "Lyumos" },
     },
     {
       path: "/services",
       name: "services",
       component: () => import("@/views/ServicesView.vue"),
+      meta: { title: "Services" },
     },
     {
       path: "/contacts",
       name: "contacts",
       component: () => import("@/views/ContactsViews.vue"),
+      meta: { title: "Contacts" },
     },
     {
       path: "/portfolio",
       name: "portfolio",
       component: () => import("@/views/PortfolioView.vue"),
+      meta: { title: "Portfolio" },
     },
     {
       path: "/services/development",
       name: "development",
       component: () => import("@/views/DevelopmentView.vue"),
+      meta: { title: "Development" },
     },
   ],
   scrollBehavior(to, from, savedPosition) {
     return savedPosition || { top: 0 };
   },
+});
+router.beforeEach((to, from, next) => {
+  document.title = `${to.meta.title}`;
+  next();
 });
 
 export default router;
