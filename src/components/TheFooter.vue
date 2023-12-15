1c403046169f7fe69ab7b5b56729c120f33323df
Alexey5689 <jjookkeerrr56@gmail.com>
2023-12-15 14:55:05 +0500

    изм. footer 1024

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,38 +1,56 @@
 <script setup>
-import { RouterLink } from 'vue-router'
+import { RouterLink } from "vue-router";
 </script>
 
-
 <template>
-  <footer class="footer" :class="{
-    'active-footer-light': leftAbsoluteActive,
-    'active-footer-dark': rightAbsoluteActive,
-  }">
+  <footer
+    class="footer"
+    :class="{
+      'active-footer-light': leftAbsoluteActive,
+      'active-footer-dark': rightAbsoluteActive,
+    }"
+  >
     <div class="wrapper">
-      <div @mouseenter="hoverLeftAbsolute" @mouseleave="noHover" class="footer__left-absolute" :class="{
-        'left-absolute-active': leftAbsoluteActive,
-        'left-absolute-inactive': leftAbsoluteInactive,
-      }">
+      <div
+        @mouseenter="hoverLeftAbsolute"
+        @mouseleave="noHover"
+        class="footer__left-absolute left-absolute-active"
+        :class="{
+          'left-absolute-active': leftAbsoluteActive,
+          'left-absolute-inactive': leftAbsoluteInactive,
+        }"
+      >
         <p class="footer__switcher-left">
           Зона активного поля <span>навигации</span>
         </p>
       </div>
 
-      <div @mouseenter="hoverRightAbsolute" @mouseleave="noHover" class="footer__right-absolute" :class="{
-        'right-absolute-active': rightAbsoluteActive,
-        'right-absolute-inactive': rightAbsoluteInactive,
-      }">
+      <div
+        @mouseenter="hoverRightAbsolute"
+        @mouseleave="noHover"
+        class="footer__right-absolute"
+        :class="{
+          'right-absolute-active': rightAbsoluteActive,
+          'right-absolute-inactive': rightAbsoluteInactive,
+        }"
+      >
         <p class="footer__switcher-right">
           Зона активного поля <span>контактов</span>
         </p>
       </div>
 
-      <span v-show="!rightAbsoluteActive && !leftAbsoluteActive" class="footer__divider"></span>
+      <span
+        v-show="!rightAbsoluteActive && !leftAbsoluteActive"
+        class="footer__divider"
+      ></span>
 
-      <div class="footer__left-side" :class="{
-        'active-left-side': leftAbsoluteActive,
-        'inactive-left-side': rightAbsoluteActive,
-      }">
+      <div
+        class="footer__left-side"
+        :class="{
+          'active-left-side': leftAbsoluteActive,
+          'inactive-left-side': rightAbsoluteActive,
+        }"
+      >
         <nav @mouseenter="hoverLeftAbsolute" @mouseleave="noHover">
           <ul>
             <li>О компании</li>
@@ -50,10 +68,15 @@ import { RouterLink } from 'vue-router'
         </p>
       </div>
 
-      <div @mouseenter="hoverRightAbsolute" @mouseleave="noHover" class="footer__right-side" :class="{
-        'active-right-side': rightAbsoluteActive,
-        'inactive-right-side': leftAbsoluteActive,
-      }">
+      <div
+        @mouseenter="hoverRightAbsolute"
+        @mouseleave="noHover"
+        class="footer__right-side"
+        :class="{
+          'active-right-side': rightAbsoluteActive,
+          'inactive-right-side': leftAbsoluteActive,
+        }"
+      >
         <div class="footer__contacts">
           <p class="footer__phone">+7 (912) 345-67-89</p>
           <p class="footer__mail">it@lumos.ru</p>
@@ -77,16 +100,23 @@ export default {
       leftAbsoluteInactive: false,
       rightAbsoluteActive: false,
       rightAbsoluteInactive: false,
+      window: {
+        width: 0,
+      },
     };
   },
   methods: {
     hoverLeftAbsolute() {
-      this.leftAbsoluteActive = true;
-      this.rightAbsoluteInactive = true;
+      this.window.width > 1024
+        ? ((this.leftAbsoluteActive = true),
+          (this.rightAbsoluteInactive = true))
+        : "";
     },
     hoverRightAbsolute() {
-      this.rightAbsoluteActive = true;
-      this.leftAbsoluteInactive = true;
+      this.window.width > 1024
+        ? ((this.rightAbsoluteActive = true),
+          (this.leftAbsoluteInactive = true))
+        : "";
     },
     noHover() {
       this.leftAbsoluteActive = false;
@@ -94,6 +124,13 @@ export default {
       this.rightAbsoluteActive = false;
       this.rightAbsoluteInactive = false;
     },
+    handleResize() {
+      this.window.width = window.innerWidth;
+    },
+  },
+  created() {
+    window.addEventListener("resize", this.handleResize);
+    this.handleResize();
   },
 };
 </script>
@@ -102,5 +139,11 @@ export default {
 .footer__left-side li a {
   text-decoration: none;
   color: #1e1e1e;
+}
+.left-absolute-active {
+  width: calc(70% - 24px);
+  margin-left: 0px;
+  -webkit-transition: 1s;
+  transition: 1s;
 }
 </style>
