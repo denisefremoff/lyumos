f37380f5024e339d1087f9675429c755184343d9
Alexey5689 <jjookkeerrr56@gmail.com>
2023-12-20 21:08:18 +0500

    2редакция

--- src/components/TheExpertise.vue
+++ src/components/TheExpertise.vue
@@ -1,31 +1,34 @@
 <template>
   <div class="expertise">
-    <div class="type_expertise">
+    <div
+      class="type_expertise"
+      v-for="(expertData, index) in expertise.expertiseData"
+      :key="expertData.id"
+    >
       <div class="type_expertise_header">
-        <h5>{{ expertise.cart1.header }}</h5>
+        <h5>{{ expertData.header }}</h5>
         <div class="type_expertise_description">
-          <div v-for="(cart, index) in expertise.cart1.body" :key="cart.id">
-            <div :class="'description_' + cart.id" v-if="cart.id === 2">
-              <h6>{{ cart.header }}</h6>
-              <p>{{ cart.body }}</p>
-            </div>
+          <div
+            v-for="exDa in expertData.body"
+            :key="exDa.id"
+            :class="'description_' + exDa.id"
+          >
+            <h6>{{ exDa.header }}</h6>
+            <p>{{ exDa.body }}</p>
+            <!-- <h6>{{ expertise.cart1.header }}</h6>
+            <p>{{ expertise.cart1.body }}</p> -->
           </div>
         </div>
       </div>
       <div class="type_expertise_footer">
-        <!-- class="expertise_active" -->
         <ul>
-          <li @click="expertise.change(1)">
-            <p>DSS</p>
-          </li>
-          <li @click="expertise.change(2)">
-            <p>EIS</p>
-          </li>
-          <li @click="expertise.change(3)">
-            <p>FIM</p>
-          </li>
-          <li @click="expertise.change(4)">
-            <p>CRM</p>
+          <!-- @click="console.log(switcher, switcher.id, index)" -->
+          <li
+            v-for="switcher in expertData.switchers"
+            :key="switcher.id"
+            @click="expertise.change(index, switcher.id)"
+          >
+            <p>{{ switcher.body }}</p>
           </li>
         </ul>
       </div>
--- src/stores/data-expertise.js
+++ src/stores/data-expertise.js
@@ -2,230 +2,188 @@ import { defineStore } from "pinia";
 export const DataExpertise = defineStore("data-expertise", {
   state: () => ({
     cart1: {
-      id: 1,
-      page: 1,
-      header: "Стратегия",
-      body: [
-        {
-          id: 1,
-          header: "DSS (Decision Support System)",
-          body: "Математические модели и статистические методы",
-        },
-        {
-          id: 2,
-          header: "EIS (Executive Information System)",
-          body: "Мониторинг ключевых показателей",
-        },
-        {
-          id: 3,
-          header: "FIM (Financial Information Management)",
-          body: "Бюджетирование и финансовый учет",
-        },
-        {
-          id: 4,
-          header: "CRM (Customer Relationship Management)",
-          body: "Маркетинг и продажи",
-        },
-      ],
-      switchers: [
-        {
-          id: 1,
-          body: "DSS",
-        },
-        {
-          id: 2,
-          body: "EIS",
-        },
-        {
-          id: 3,
-          body: "FIM",
-        },
-        {
-          id: 4,
-          body: "CRM",
-        },
-      ],
+      header: "FIM (Financial Information Management)",
+      body: "Бюджетирование и финансовый учет",
     },
 
-    // expertiseData: [
-    //   {
-    //     id: 1,
-    //     header: "Стратегия",
-    //     body: [
-    //       {
-    //         id: 1,
-    //         header: "DSS (Decision Support System)",
-    //         body: "Математические модели и статистические методы",
-    //         isShow: true,
-    //       },
-    //       {
-    //         id: 2,
-    //         header: "EIS (Executive Information System)",
-    //         isShow: false,
-    //       },
-    //       {
-    //         id: 3,
-    //         header: "FIM (Financial Information Management)",
-    //         isShow: false,
-    //       },
-    //       {
-    //         id: 4,
-    //         header: "CRM (Customer Relationship Management)",
-    //         isShow: false,
-    //       },
-    //     ],
-    //     switchers: [
-    //       {
-    //         id: 1,
-    //         body: "DSS",
-    //       },
-    //       {
-    //         id: 2,
-    //         body: "EIS",
-    //       },
-    //       {
-    //         id: 3,
-    //         body: "FIM",
-    //       },
-    //       {
-    //         id: 4,
-    //         body: "CRM",
-    //       },
-    //     ],
-    //   },
-    //   {
-    //     id: 2,
-    //     header: "Производство",
-    //     body: [
-    //       {
-    //         id: 1,
-    //         header: "SCM (Supply Chain Management)",
-    //         body: "Поставки и логистика",
-    //       },
-    //       //   {
-    //       //     id: 2,
-    //       //     header: "2",
-    //       //   },
-    //       //   {
-    //       //     id: 3,
-    //       //     header: "3",
-    //       //   },
-    //       //   {
-    //       //     id: 4,
-    //       //     header: "4",
-    //       //   },
-    //     ],
-    //     switchers: [
-    //       {
-    //         id: 1,
-    //         body: "SCM",
-    //       },
-    //       {
-    //         id: 2,
-    //         body: "WMS",
-    //       },
-    //       {
-    //         id: 3,
-    //         body: "ERP",
-    //       },
-    //       {
-    //         id: 4,
-    //         body: "MES",
-    //       },
-    //     ],
-    //   },
-    //   {
-    //     id: 3,
-    //     header: "Персонал",
-    //     body: [
-    //       {
-    //         id: 1,
-    //         header: "HRM (Human Resource Management)",
-    //         body: "Управление персоналом",
-    //       },
-    //         {
-    //           id: 2,
-    //           header: "2",
-    //         },
-    //         {
-    //           id: 3,
-    //           header: "3",
-    //         },
-    //         {
-    //           id: 4,
-    //           header: "4",
-    //         },
-    //         {
-    //           id: 5,
-    //           header: "4",
-    //         },
-    //     ],
-    //     switchers: [
-    //       {
-    //         id: 1,
-    //         body: "HRM",
-    //       },
-    //       {
-    //         id: 2,
-    //         body: "LMS",
-    //       },
-    //       {
-    //         id: 3,
-    //         body: "PM",
-    //       },
-    //       {
-    //         id: 4,
-    //         body: "BPM",
-    //       },
-    //       {
-    //         id: 5,
-    //         body: "OAS",
-    //       },
-    //     ],
-    //   },
-    //   {
-    //     id: 4,
-    //     header: "Информация",
-    //     body: [
-    //       {
-    //         id: 1,
-    //         header: "ECM (Enterprise Content Management)",
-    //         body: "Прием и обработка документов",
-    //       },
-    //         {
-    //           id: 2,
-    //           header: "2",
-    //         },
-    //         {
-    //           id: 3,
-    //           header: "3",
-    //         },
-    //         {
-    //           id: 4,
-    //           header: "4",
-    //         },
-    //     ],
-    //     switchers: [
-    //       {
-    //         id: 1,
-    //         body: "ECM",
-    //       },
-    //       {
-    //         id: 2,
-    //         body: "EDI",
-    //       },
-    //     ],
-    //   },
-    // ],
+    expertiseData: [
+      {
+        id: 1,
+        header: "Стратегия",
+        body: [
+          {
+            id: 1,
+            header: "DSS (Decision Support System)",
+            body: "Математические модели и статистические методы",
+          },
+          {
+            id: 2,
+            header: "EIS (Executive Information System)",
+            body: "Мониторинг ключевых показателей",
+          },
+          {
+            id: 3,
+            header: "FIM (Financial Information Management)",
+            body: "Бюджетирование и финансовый учет",
+          },
+          {
+            id: 4,
+            header: "CRM (Customer Relationship Management)",
+            body: "Маркетинг и продажи",
+          },
+        ],
+        switchers: [
+          {
+            id: 1,
+            body: "DSS",
+          },
+          {
+            id: 2,
+            body: "EIS",
+          },
+          {
+            id: 3,
+            body: "FIM",
+          },
+          {
+            id: 4,
+            body: "CRM",
+          },
+        ],
+      },
+      {
+        id: 2,
+        header: "Производство",
+        body: [
+          {
+            id: 1,
+            header: "SCM (Supply Chain Management)",
+            body: "Поставки и логистика",
+          },
+          //   {
+          //     id: 2,
+          //     header: "2",
+          //   },
+          //   {
+          //     id: 3,
+          //     header: "3",
+          //   },
+          //   {
+          //     id: 4,
+          //     header: "4",
+          //   },
+        ],
+        switchers: [
+          {
+            id: 1,
+            body: "SCM",
+          },
+          {
+            id: 2,
+            body: "WMS",
+          },
+          {
+            id: 3,
+            body: "ERP",
+          },
+          {
+            id: 4,
+            body: "MES",
+          },
+        ],
+      },
+      {
+        id: 3,
+        header: "Персонал",
+        body: [
+          {
+            id: 1,
+            header: "HRM (Human Resource Management)",
+            body: "Управление персоналом",
+          },
+          // {
+          //   id: 2,
+          //   header: "2",
+          // },
+          // {
+          //   id: 3,
+          //   header: "3",
+          // },
+          // {
+          //   id: 4,
+          //   header: "4",
+          // },
+          // {
+          //   id: 5,
+          //   header: "4",
+          // },
+        ],
+        switchers: [
+          {
+            id: 1,
+            body: "HRM",
+          },
+          {
+            id: 2,
+            body: "LMS",
+          },
+          {
+            id: 3,
+            body: "PM",
+          },
+          {
+            id: 4,
+            body: "BPM",
+          },
+          {
+            id: 5,
+            body: "OAS",
+          },
+        ],
+      },
+      {
+        id: 4,
+        header: "Информация",
+        body: [
+          {
+            id: 1,
+            header: "ECM (Enterprise Content Management)",
+            body: "Прием и обработка документов",
+          },
+          // {
+          //   id: 2,
+          //   header: "2",
+          // },
+          // {
+          //   id: 3,
+          //   header: "3",
+          // },
+          // {
+          //   id: 4,
+          //   header: "4",
+          // },
+        ],
+        switchers: [
+          {
+            id: 1,
+            body: "ECM",
+          },
+          {
+            id: 2,
+            body: "EDI",
+          },
+        ],
+      },
+    ],
   }),
   getters: {
     page() {
-      return this.cart1.page;
+      return this.cart1;
     },
   },
   actions: {
-    change(n) {
-      this.cart1.page = n;
+    change(index, n) {
+      this.cart1 = this.expertiseData[index].body[n - 1];
     },
   },
 });
