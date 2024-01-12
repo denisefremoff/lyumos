4f23261dfc34d42765bc33aa4a9e49314a104ef9
denisefremoff <denisefremoff@mail.ru>
2024-01-12 17:23:54 +0500

    модальное окно формы

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <script setup>
 import { RouterView } from "vue-router";
-import TheForm from "@/components/TheForm.vue";
+// import TheForm from "@/components/TheForm.vue";
 import { FormControl } from "@/stores/form-control.js";
 const form = FormControl();
 </script>
 
 <template>
-  <teleport to="body"> <TheForm v-if="form.getFormState" /></teleport>
+  <!-- <teleport to="body"> 
+    <TheForm v-if="form.getFormState" />
+  </teleport> -->
   <RouterView />
+  
 </template>
 
 <style scoped></style>
