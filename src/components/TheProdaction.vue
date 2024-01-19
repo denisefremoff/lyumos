211821c969defadcdb55569472481520c4993061
Alexey5689 <jjookkeerrr56@gmail.com>
2024-01-19 17:25:49 +0500

    mouseenter

--- src/components/TheProdaction.vue
+++ src/components/TheProdaction.vue
@@ -10,7 +10,17 @@
       </div>
     </div>
     <div class="type_expertise_footer">
-      <ul>
+      <ul v-if="width > 1023">
+        <li
+          v-for="(switcher, index) in expertise.cart2.switchers"
+          :key="switcher.id"
+          @mouseenter="expertise.changeCart2(index)"
+          :class="{ expertise_active: switcher.id === expertise.pageCart2 + 1 }"
+        >
+          <p>{{ switcher.body }}</p>
+        </li>
+      </ul>
+      <ul v-else>
         <li
           v-for="(switcher, index) in expertise.cart2.switchers"
           :key="switcher.id"
@@ -26,4 +36,22 @@
 <script setup>
 import { DataExpertise } from "@/stores/data-expertise.js";
 const expertise = DataExpertise();
+</script>
+<script>
+export default {
+  data() {
+    return {
+      width: 0,
+    };
+  },
+  methods: {
+    handleResize() {
+      this.width = window.innerWidth;
+    },
+  },
+  mounted() {
+    window.addEventListener("resize", this.handleResize);
+    this.handleResize();
+  },
+};
 </script>
--- src/components/TheStaff.vue
+++ src/components/TheStaff.vue
@@ -10,7 +10,17 @@
       </div>
     </div>
     <div class="type_expertise_footer">
-      <ul>
+      <ul v-if="width > 1023">
+        <li
+          v-for="(switcher, index) in expertise.cart3.switchers"
+          :key="switcher.id"
+          @mouseenter="expertise.changeCart3(index)"
+          :class="{ expertise_active: switcher.id === expertise.pageCart3 + 1 }"
+        >
+          <p>{{ switcher.body }}</p>
+        </li>
+      </ul>
+      <ul v-else>
         <li
           v-for="(switcher, index) in expertise.cart3.switchers"
           :key="switcher.id"
@@ -27,4 +37,22 @@
 <script setup>
 import { DataExpertise } from "@/stores/data-expertise.js";
 const expertise = DataExpertise();
+</script>
+<script>
+export default {
+  data() {
+    return {
+      width: 0,
+    };
+  },
+  methods: {
+    handleResize() {
+      this.width = window.innerWidth;
+    },
+  },
+  mounted() {
+    window.addEventListener("resize", this.handleResize);
+    this.handleResize();
+  },
+};
 </script>
