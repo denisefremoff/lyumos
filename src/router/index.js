1a3893462dccb62867f92c4cfb6013e1a8114922
denisefremoff <denisefremoff@mail.ru>
2024-01-23 17:47:11 +0500

    правка адреса

--- src/router/index.js
+++ src/router/index.js
@@ -2,14 +2,13 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
 
 const router = createRouter({
-  // history: createWebHistory(import.meta.env.BASE_URL),
   history: createWebHistory('/'),
   routes: [
     {
       path: "/",
       name: "home",
       component: HomeView,
-      meta: { title: "Lyumos" },
+      meta: { title: "Lymos" },
     },
     {
       path: "/services",
